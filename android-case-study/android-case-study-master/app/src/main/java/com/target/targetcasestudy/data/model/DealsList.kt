2e84f9ff4eb500e3c2c7a47b093fab969157a7b9
Bhargava chowdary <bhargava.musunuru@gmail.com>
2021-03-14 22:09:36 -0400

    Updated package

--- android-case-study/android-case-study-master/app/src/main/java/com/target/targetcasestudy/data/model/DealsList.kt
+++
@@ -1,9 +0,0 @@
-package com.target.targetcasestudy.data.model
-
-
-import com.google.gson.annotations.SerializedName
-
-data class DealsList(
-    @SerializedName("products")
-    val products: List<DealItem>
-)
--- android-case-study/android-case-study-master/app/src/main/java/com/target/targetcasestudy/requets/data/DealsList.kt
+++
@@ -1,9 +0,0 @@
-package com.target.targetcasestudy.requets.data
-
-
-import com.google.gson.annotations.SerializedName
-
-data class DealsList(
-    @SerializedName("products")
-    val products: List<DealItem>
-)
